3d16226cf5d7156de682ffd9b40af7e33e05ae9d
andreas di cesaris <andreas.decesaris1995@gmail.com>
2024-05-22 11:45:49 -0300

    minha conta terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double saldo = 00;
+
+        System.out.println("bem vindo ao Banco JK ");
+        System.out.println("digite seu nome :");
+        String nome = scanner.next();
+        System.out.println("digite seu sobrenome :");
+        String sobreNome = scanner.next();
+        System.out.println("digite sua agencia :");
+        String Agencia = scanner.next();
+        System.out.println("conta :");
+        int conta = scanner.nextInt();
+
+        System.out.println("Sr."+ nome +" " + sobreNome + " bem vindo ao nosso banco");
+        System.out.println("sua agencia é " + Agencia );
+        System.out.println("sua conta é " + conta);
+        System.out.println("seu saldo: "+ saldo + " ja esta disponivel para saque");
+
     }
 }
